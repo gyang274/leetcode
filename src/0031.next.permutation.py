539a8e94fe102945513cbee884444f2e81129149
yg <gyang274@gmail.com>
2020-01-20 00:08:21 -0500

    2020.01.19

--- src/0031.next.permutation.py
+++ src/0031.next.permutation.py
@@ -10,24 +10,17 @@ class Solution:
     i = n - 1
     while i > 0 and nums[i] <= nums[i - 1]:
       i -= 1
-    # print(i)
     if i == 0:
       for k in range(n // 2):
         nums[k], nums[n - 1 - k] = nums[n - 1 - k], nums[k]
     else:
       j = i
-      while j <= n - 1 and nums[j] >= nums[i - 1]:
+      while j < n and nums[j] > nums[i - 1]:
         j += 1
-      # print(j)
-      if j == n:
-        for k in range(i - 1, n - 1):
-          nums[k], nums[k + 1] = nums[k + 1], nums[k]
-      else:
-        nums[i - 1], nums[j - 1] = nums[j - 1], nums[i - 1]
-        for k in range((n - i) // 2):
-          # print(k)
-          nums[i + k], nums[n - 1 - k] = nums[n - 1 - k], nums[i + k]
-    return None
+      nums[i - 1], nums[j - 1] = nums[j - 1], nums[i - 1]
+      for k in range((n - i) // 2):
+        nums[i + k], nums[n - 1 - k] = nums[n - 1 - k], nums[i + k]
+    # return None
 
 
 if __name__ == '__main__':
@@ -35,15 +28,15 @@ if __name__ == '__main__':
   # test cases
   cases = [
     # [],
-    # [1],
-    # [1, 2, 3],
-    # [1, 3, 2],
-    # [3, 2, 1],
-    # [1, 1, 4],
+    [1],
+    [1, 2, 3],
+    [1, 3, 2],
+    [3, 2, 1],
+    [1, 1, 4],
     [1, 4, 1],
-    # [4, 1, 1],
-    # [1, 3, 4, 2],
-    # [1, 4, 3, 2],
+    [4, 1, 1],
+    [1, 3, 4, 2],
+    [1, 4, 3, 2],
   ]
   rslts = [solver.nextPermutation(nums) for nums in cases]
   for cs, rs in zip(cases, rslts):
