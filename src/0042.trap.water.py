e0bbe883f14546cf73a7179714a320cdaae41274
yg <gyang274@gmail.com>
2020-01-24 10:43:55 -0500

    2020.01.24

--- src/0042.trap.water.py
+++ src/0042.trap.water.py
@@ -1,26 +1,51 @@
 from typing import List
 
 
+# class Solution:
+#   def trap(self, height: List[int]) -> int:
+#     """
+#     Maintain 2 list of partial potential views, 
+#       one scan from left to right, represent potential from lft view, 
+#       one scan from right to left, represent potential from rgt view, 
+#     Then, take the min at each position, O(3n).
+#     """
+#     l = [0 for _ in height]
+#     r = [0 for _ in height]
+#     lhmax = -1
+#     for i, h in enumerate(height):
+#       lhmax = max(lhmax, h)
+#       l[i] = lhmax - h
+#     rhmax = -1
+#     for i, h in reversed(list(enumerate(height))):
+#       rhmax = max(rhmax, h)
+#       r[i] = rhmax - h
+#     return sum([min(lh, rh) for lh, rh in zip(l, r)])
+
+
 class Solution:
   def trap(self, height: List[int]) -> int:
+    """Two pointers.
     """
-    Maintain 2 list of partial potential views, 
-      one scan from left to right, represent potential from lft view, 
-      one scan from right to left, represent potential from rgt view, 
-    Then, take the min at each position, O(3n).
-    """
-    l = [0 for _ in height]
-    r = [0 for _ in height]
-    lhmax = -1
-    for i, h in enumerate(height):
-      lhmax = max(lhmax, h)
-      l[i] = lhmax - h
-    rhmax = -1
-    for i, h in reversed(list(enumerate(height))):
-      rhmax = max(rhmax, h)
-      r[i] = rhmax - h
-    return sum([min(lh, rh) for lh, rh in zip(l, r)])
-
+    li = 0
+    ri = len(height) - 1
+    lh = -1
+    rh = -1
+    w = 0
+    while li < ri:
+      if height[li] >= height[ri]:
+        if height[ri] >= rh:
+          rh = height[ri]
+        else:
+          w += rh - height[ri]
+        ri -= 1
+      else:
+        if height[li] >= lh:
+          lh = height[li]
+        else:
+          w += lh - height[li]
+        li += 1
+    return w
+    
 
 if __name__ == '__main__':
   solver = Solution()
---
+++ src/0407.trap.water.2.py
@@ -0,0 +1,68 @@
+import heapq
+from typing import List
+
+
+class Solution:
+  def trapRainWater(self, heightMap: List[List[int]]) -> int:
+    """Priority queue move the boundary to track water, O(4?mn). 
+    """
+    m = len(heightMap)
+    if m < 3:
+      return 0
+    n = len(heightMap[0])
+    if n < 3:
+      return 0
+    # push all boundary into a priority queue
+    bound = []
+    visit = set()
+    w = 0
+    for i in [0, m - 1]:
+      for j in range(n):
+        bound.append((heightMap[i][j], i, j))
+        visit.add((i, j))
+    for j in [0, n - 1]:
+      for i in range(1, m - 1):
+        bound.append((heightMap[i][j], i, j))
+        visit.add((i, j))
+    # heapify
+    heapq.heapify(bound)
+    # move bound
+    hbnd = -1
+    while bound:
+      hmin, x, y = heapq.heappop(bound)
+      hbnd = max(hbnd, hmin)
+      dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+      for dx, dy in dxy:
+        nx = x + dx
+        ny = y + dy
+        if (not (0 <= nx < m and 0 <= ny < n)) or ((nx, ny) in visit):
+          continue
+        h = heightMap[nx][ny]
+        if h < hbnd:
+          w += hbnd - h
+        heapq.heappush(bound, (h, nx, ny))
+        visit.add((nx, ny))
+        # print(f'visit: {nx=}, {ny=}, {h=}, {hbnd=}, {hmin=}, {w=}')
+    return w
+    
+
+if __name__ == '__main__':
+  solver = Solution()
+  # test cases
+  cases = [
+    [
+      [1,4,3,1,3,2],
+      [3,2,1,3,2,4],
+      [2,3,3,2,3,1],
+    ],
+    [
+      [12,13,1,12],
+      [13,4,13,12],
+      [13,8,10,12],
+      [12,13,12,12],
+      [13,13,13,13]
+    ],
+  ]
+  rslts = [solver.trapRainWater(heightMap) for heightMap in cases]
+  for cs, rs in zip(cases, rslts):
+    print(f"case: {cs} | solution: {rs}")
