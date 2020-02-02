51bd7cc2bed26f42ef9c18816108f56af2be45dc
yg <gyang274@gmail.com>
2020-02-02 13:43:23 -0500

    2020.02.02

---
+++ src/0098.validate.bst.py
@@ -0,0 +1,41 @@
+from config.treenode import TreeNode, listToTreeNode
+
+
+class Solution:
+  def isValidBST(self, root: TreeNode) -> bool:
+    """Approaches:
+      1. Recursive
+      2. Iterative
+      3. Inorder traversal leads to a sorted list.
+    """
+    if root is not None:
+      x, node, stack = float('-inf'), root, []
+      while node or stack:
+        while node:
+          stack.append(node)
+          node = node.left
+        node = stack.pop()
+        if x < node.val:
+          x = node.val
+          node = node.right
+        else:
+          return False
+    return True
+
+
+if __name__ == '__main__':  
+  solver = Solution()
+  cases = [
+    [],
+    [1],
+    [2, 1, 3],
+    [1, None, 2, 3],
+  ]
+  cases = [
+    (listToTreeNode(x)) for x in cases
+  ]
+  rslts = [
+    solver.isValidBST(x) for x in cases
+  ]
+  for cs, rs in zip(cases, rslts):
+    print(f"case:\n{cs.display() if cs is not None else cs}, solution: {rs}")
