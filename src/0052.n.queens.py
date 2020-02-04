0c604fe58c2e5e311dbff4f04f5fab1c7e02c8ed
yg <gyang274@gmail.com>
2020-02-03 23:02:40 -0500

    2020.02.03

---
+++ src/0052.n.queens.py
@@ -0,0 +1,41 @@
+class Solution:
+  def backtrack(self, rows, cols, hill, rowNext, dale, n):
+    if rows == n:
+      self.count += 1
+    else:
+      colLeft = cols & ~(hill | rowNext | dale)
+      while colLeft:
+        colTook = -colLeft & colLeft
+        colLeft ^= colTook
+        self.backtrack(rows + 1, cols, (hill | colTook) << 1, rowNext | colTook, (dale | colTook) >> 1, n)
+  def totalNQueens(self, n: int) -> int:
+    """backtrack
+      improvement 1: use row, col, hill and dale diagonals to replace board
+        row: a list of n, at each index i, store the i-th row's queen position
+        col: a list of n, at each index j, store whether or not j-th col has been taken.
+        hill, dale: a list of (2n - 1), at each index k, store whether or not k-th diagonal has been taken.
+      improvement 2: use bitmap to represent row, col, hill and dale diagonals
+    """
+    self.count, rows, cols, hill, rowNext, dale = 0, 0, (1 << n) - 1, 0, 0, 0
+    self.backtrack(rows, cols, hill,rowNext, dale, n)
+    return self.count
+
+
+if __name__ == '__main__':
+  solver = Solution()
+  # test cases
+  cases = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+  ]
+  rslts = [solver.totalNQueens(n) for n in cases]
+  for cs, rs in zip(cases, rslts):
+    print(f"case: {cs} | solution: {rs}")
