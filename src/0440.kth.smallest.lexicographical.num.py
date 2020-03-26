24425d8ab120032493e7ab8f5d57d3ecdb5df372
yg <gyang274@gmail.com>
2020-03-26 14:49:23 -0400

    2020.03.26

--- src/0440.kth.smallest.lexicographical.num.py
+++ src/0440.kth.smallest.lexicographical.num.py
@@ -27,10 +27,34 @@ class Solution:
       self.recursive(z, 0)
     return self.ans[k - 1]
 
+class Solution:
+  def _dist_to_next(self, x, y, n):
+    """dist from x to y, x and y with same num of digits, given n.
+    """
+    count = 0
+    while x <= n:
+      count += min(n + 1, y) - x
+      x *= 10
+      y *= 10
+    return count
+  def findKthNumber(self, n: int, k: int) -> int:
+    x = 1
+    while k > 1:
+      d = self._dist_to_next(x, x + 1, n)
+      # print(f"{n=}, {k=}, {x=}, {d=}")
+      if d > k - 1:
+        x *= 10
+        k -= 1
+      else:
+        k -= d
+        x += 1
+    return x
+
 if __name__ == '__main__':
   solver = Solution()
   cases = [
-    (13, 2),
+    (10, 3),
+    (14, 2),
     (4089173, 3098723),
   ]
   rslts = [solver.findKthNumber(n, k) for n, k in cases]
