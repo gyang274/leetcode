0a075ad4cffce09e6ba40040d72a04ef171381bb
yg <gyang274@gmail.com>
2020-06-11 23:16:03 -0400

    2020.06.11

---
+++ src/0869.reorder.power.of.2.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+  def __init__(self):
+    self.s = {tuple((k, v) for k, v in sorted(Counter(str(1 << i)).items())) for i in range(32)}   
+  def reorderedPowerOf2(self, N: int) -> bool:
+    return tuple((k, v) for k, v in sorted(Counter(str(N)).items())) in self.s
+
+        
