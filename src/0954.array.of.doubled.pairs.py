ee1178625652c7a20caa3d77dcbcf811c601c5ab
yg <gyang274@gmail.com>
2020-07-06 19:49:34 -0400

    2020.07.06

---
+++ src/0954.array.of.doubled.pairs.py
@@ -0,0 +1,36 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+  def canReorderDoubled(self, A: List[int]) -> bool:
+    A.sort()
+    d = Counter(A)
+    for x in A:
+      if d[x] > 0:
+        if x == 0:
+          if d[x] & 1:
+            return False
+        if x > 0:
+          if d[x * 2] >= d[x]:
+            d[x * 2] -= d[x]
+          else:
+            return False
+        else:
+          if ((x & 1) ^ 1) and d[x // 2] >= d[x]:
+            d[x // 2] -= d[x]
+          else:
+            return False
+        d[x] = 0
+    return True
+
+if __name__ == '__main__':
+  solver = Solution()
+  cases = [
+    [2,1,2,4],
+    [3,2,4,8],
+    [4,-2,2,-4],
+    [1,2,4,16,8,4],
+  ]
+  rslts = [solver.canReorderDoubled(A) for A in cases]
+  for cs, rs in zip(cases, rslts):
+    print(f"case: {cs} | solution: {rs}")
