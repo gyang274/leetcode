10a9fc7390018ca7e6de27b0f84913999454f658
yg <gyang274@gmail.com>
2020-10-22 17:41:04 -0400

    2020.10.22

---
+++ src/1450.num.students.homework.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+  def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+    return sum(x <= queryTime <= y for x, y in zip(startTime, endTime))
