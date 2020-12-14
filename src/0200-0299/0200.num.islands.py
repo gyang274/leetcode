5cdffe3525453d732575201f05d55ac319785736
yg <gyang274@gmail.com>
2020-12-13 22:49:11 -0500

    2020.12.13

--- src/0200-0299/0200.num.islands.py
+++ src/0200-0299/0200.num.islands.py
@@ -27,6 +27,34 @@ class Solution:
                 bounary.add((i, j))
     return islands
 
+class DSU:
+  def __init__(self, reps: dict = {}):
+    # representer
+    self.reps = reps
+  def add(self, x):
+    self.reps[x] = x
+  def find(self, x):
+    if not x == self.reps[x]:
+      self.reps[x] = self.find(self.reps[x])
+    return self.reps[x]
+  def union(self, x, y):
+    self.reps[self.find(y)] = self.find(x)
+
+class Solution:
+  def numIslands(self, grid: List[List[str]]) -> int:
+    m, n = len(grid), len(grid[0])
+    # disjoint set union
+    dsu = DSU(reps={})
+    for i in range(m):
+      for j in range(n):
+        if grid[i][j] == "1":
+          dsu.add((i, j))
+          if i > 0 and grid[i][j] == grid[i - 1][j] == "1":
+            dsu.union((i - 1, j), (i, j))
+          if j > 0 and grid[i][j] == grid[i][j - 1] == "1":
+            dsu.union((i, j - 1), (i, j))
+    return len(set([dsu.find(k) for k in dsu.reps]))
+
 if __name__ == '__main__':
   solver = Solution()
   cases = [
